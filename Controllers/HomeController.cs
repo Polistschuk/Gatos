2fd6235c8bd32aef4d1eb4d09afaf57998931a94
Julio Polistschuk <juliopolistschukw@gmail.com>
2022-01-20 22:42:56 -0300

    Baixa ai

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PrimeiroProjeto.Controllers;
+
+public class HomeController : Controller
+{
+  
+
+    public string Index()
+    {
+        return "Em prol dos gatos gordos";
+    }
+
+   
+}
